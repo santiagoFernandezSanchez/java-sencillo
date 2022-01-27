2ca50e338f8506e6d938ff54f90e6b4c22ef1043
Santiago Fernandez Sanchez <sfersan584@g.educaand.es>
2022-01-27 09:16:49 +0100

    Creando el proyecto maven

---
+++ src/main/java/com/santiago/App.java
@@ -0,0 +1,13 @@
+package com.santiago;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
